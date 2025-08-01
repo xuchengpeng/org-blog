7e05e91c7c7763b09f20c7f914d0ea3e2678f704
xuchengpeng <chuckxcp@qq.com>
2025-08-01 16:06:55 +0800

    add notes

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ const path = require('path');
 const cheerio = require('cheerio');
 
 async function parseHtmls() {
-  const inputDir = './public/posts';
   const entries = [];
 
   try {
-    const files = await fs.readdir(inputDir);
-    for (const file of files) {
-      if (file !== 'index.html' && path.extname(file).toLowerCase() === '.html') {
-        const filePath = path.join(inputDir, file);
-        try {
-          const htmlContent = await fs.readFile(filePath, 'utf8');
-          const $ = cheerio.load(htmlContent);
-          const title = $('title').text();
-          const description = $('meta[name=description]').attr('content');
-          const keywords = $('meta[name=keywords]').attr('content');
-          const url = '/posts/' + file;
-          entries.push({
-            title: title || '',
-            description: description || '',
-            keywords: keywords || '',
-            url: url
-          });
-        } catch (readErr) {
-          console.error(`Error reading or parsing ${file}:`, readErr.message);
+    for (const dir of ["posts", "notes"]) {
+      const inputDir = './public/' + dir;
+      const files = await fs.readdir(inputDir);
+      for (const file of files) {
+        if (file !== 'index.html' && path.extname(file).toLowerCase() === '.html') {
+          const filePath = path.join(inputDir, file);
+          try {
+            const htmlContent = await fs.readFile(filePath, 'utf8');
+            const $ = cheerio.load(htmlContent);
+            const title = $('title').text();
+            const description = $('meta[name=description]').attr('content');
+            const keywords = $('meta[name=keywords]').attr('content');
+            const url = '/' + dir + '/' + file;
+            entries.push({
+              title: title || '',
+              description: description || '',
+              keywords: keywords || '',
+              url: url
+            });
+          } catch (readErr) {
+            console.error(`Error reading or parsing ${file}:`, readErr.message);
+          }
         }
       }
     }
